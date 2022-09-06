f098e4c788abc5daf7afce3ebd189b1cf0eb135e
ulvikrml <ulvikrml@gmail.com>
2022-09-07 01:16:07 +0400

    added localstorage

--- src/components/ToDoList.jsx
+++ src/components/ToDoList.jsx
@@ -1,4 +1,4 @@
-import React, {useState} from 'react'
+import React, { useState, useEffect } from 'react'
 import './ToDoList.css'
 import AddNewToDo from './AddNewToDo';
 import DataList from './DataList';
@@ -6,24 +6,46 @@ import ToDoFooter from './ToDoFooter';
 
 function ToDoList() {
   const [items, setItems] = useState([]);
-  const setToDoItems = (data) =>{
+  const setToDoItems = (data) => {
     setItems(prevState => {
       return [...prevState, data];
     });
   }
-  const deleteItem = (id) =>{
+  const localStorageItems = (set) => {
+    localStorage.setItem('data', JSON.stringify(set));
+
+  }
+  const deleteItem = (id) => {
     setItems(items.filter(data => data.id !== id));
+    if (items.length === 1) {
+      localStorageItems([]);
+    }
   }
-  const clearAllItems = () =>{
+  const clearAllItems = () => {
     setItems([]);
+    localStorageItems([])
   }
-  
+
+  useEffect(() => {
+    if (!(items.length === 0)) {
+      localStorageItems(items)
+    }
+  }, [items]);
+
+  useEffect(() => {
+    const data = JSON.parse(localStorage.getItem('data'));
+
+    if (data) {
+      setItems(data);
+    }
+  }, []);
+
   return (
     <div className='todo-container'>
-        <h2>Todo App</h2>
-        <AddNewToDo setToDoItems={setToDoItems}/>
-        <DataList items={items} deleteItem={deleteItem}/>
-        <ToDoFooter length={items.length} clearAll={clearAllItems}/>
+      <h2>Todo App</h2>
+      <AddNewToDo setToDoItems={setToDoItems} />
+      <DataList items={items} deleteItem={deleteItem} />
+      <ToDoFooter length={items.length} clearAll={clearAllItems} />
     </div>
   )
 }
